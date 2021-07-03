f9927f1412ec6420288f91f0af81431169558557
tlqvst <erik@tellqvist.net>
2021-07-03 15:03:43 +0200

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div className="m-8">
+     <h1 className="text-3xl">Hello World.</h1>
+     <p>Nextjs + Sass + Tailwind = 😻</p>
+   </div>
+
+  )
+}
